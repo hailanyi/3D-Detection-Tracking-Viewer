2384f4cd43b5accb38c50384f77ae8a3ef6bfb98
hailanyi <869255907@qq.com>
2021-12-10 09:41:17 +0800

    Update kitti_3D_tracking_viewer.py

--- kitti_3D_tracking_viewer.py
+++ kitti_3D_tracking_viewer.py
@@ -18,7 +18,7 @@ def kitti_viewer():
             labels = labels[mask]
             label_names = label_names[mask]
             vi.add_3D_boxes(labels, ids=labels[:, -1].astype(int), box_info=label_names,caption_size=(0.09,0.09))
-            vi.add_3D_cars(labels, mesh_alpha=1)
+            vi.add_3D_cars(labels, ids=labels[:, -1].astype(int), mesh_alpha=1)
         vi.add_points(points[:,:3])
 
         vi.add_image(image)
